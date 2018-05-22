7896dd64f90d91de9307fc1c17bfed9a5dee9172
Kai Chen <kaiche2@cisco.com>
2018-05-22 11:00:38 -0700

    Added validation, updated Review model, added new Review api

---
+++ validation/review.js
@@ -0,0 +1,17 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateReviewInput (data) {
+    let errors = {};
+
+    data.movieTitle = !isEmpty(data.movieTitle) ? data.movieTitle : '';
+
+    if(!Validator.isLength(data.movieTitle, {min: 3, max: 30})) {
+        errors.movieTitle = 'Movie Title must be between 3 and 30 characters';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+};

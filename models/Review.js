751be24532918f4deb880134a36ea0c9d7293de2
Kai Chen <kaiche2@cisco.com>
2018-07-09 18:08:41 -0700

    Added score attribute to Review model, updated review validation

--- models/Review.js
+++ models/Review.js
@@ -1,55 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Review Schema
 const ReviewSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    movieTitle: {
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  score: {
+    type: String
+  },
+  movieTitle: {
+    type: String,
+    required: true
+  },
+  pros: [
+    {
+      _id: false,
+      id: {
+        type: Number,
+        required: true
+      },
+      text: {
         type: String,
         required: true
-    },
-    pros: [
-        {
-            _id : false,
-            id: {
-                type: Number,
-                required: true
-            },
-            text: {
-                type: String,
-                required: true
-            }
-        }
-    ],
-    cons: [{
-        _id : false,
-        id: {
-            type: Number,
-            required: true
-        },
-        text: {
-            type: String,
-            required: true
-        }
-    }],
-    other: [{
-        _id : false,
-        id: {
-            type: Number,
-            required: true
-        },
-        text: {
-            type: String,
-            required: true
-        }
-    }]
+      }
+    }
+  ],
+  cons: [
+    {
+      _id: false,
+      id: {
+        type: Number,
+        required: true
+      },
+      text: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  other: [
+    {
+      _id: false,
+      id: {
+        type: Number,
+        required: true
+      },
+      text: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
-mongoose.model('reviews', ReviewSchema);
+mongoose.model("reviews", ReviewSchema);
